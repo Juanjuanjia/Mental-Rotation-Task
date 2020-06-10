f8e02fbf2e909a7e8b6181d31d17c65f4a789ded
Juanjuanjia <18556317+Juanjuanjia@users.noreply.github.com>
2020-06-10 11:35:51 +0200

    first commit

---
+++ 04_trials.js
@@ -0,0 +1,578 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    key_press: [
+        {
+            picture: 'images/1_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/1_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/1_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/2_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/2_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/3_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/3_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/4_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/2_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/2_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/5_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/5_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        
+    ]
+};
+
+const main_info = {
+    key_press: [
+        {
+            picture: 'images/6_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/6_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/3_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/3_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/7_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/7_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/8_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/8_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/9_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/9_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/4_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/4_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/10_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/10_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/11_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/11_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/12_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/12_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/13_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/13_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/14_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/14_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/15_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/15_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/5_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/5_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/6_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/6_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/7_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/7_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/8_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/8_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/9_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/9_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/10_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/10_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/11_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/11_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/12_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/12_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/13_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/13_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/14_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/14_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/15_150_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/15_150_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+        {
+            picture: 'images/10_50_different.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different'
+        },
+        {
+            picture: 'images/10_50_same.jpg',
+            question: "Are they same ('s') or different ('d')?",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same'
+        },
+
+    ]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
